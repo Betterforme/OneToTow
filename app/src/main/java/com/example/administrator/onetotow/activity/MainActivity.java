1ea65fe03ef73d657a4680aa3b60b8c6f0802eaf
黑牛哞哞 <763209836@qq.com>
2016-11-04 17:27:40 +0800

    完善工程结构

--- app/src/main/java/com/example/administrator/onetotow/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/onetotow/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.administrator.onetotow;
+package com.example.administrator.onetotow.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.administrator.onetotow.R;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
